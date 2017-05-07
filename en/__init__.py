d4238760051f8507bbfd3b48a9b7e437fb4b0787
Callum Oakley <callum@pusher.com>
2017-05-07 10:51:11 +0100

    added compositions of list and find and open and cat

--- en/__init__.py
+++ en/__init__.py
@@ -1,5 +1,5 @@
 from os import getenv, listdir
-from subprocess import call
+from subprocess import call, check_output
 
 # Would be nice to get these from a config file
 root = "{}/notes".format(getenv("HOME"))
@@ -19,6 +19,21 @@ def edit(notes):
         command.append(path(note))
     call(command)
 
+def separator():
+    return "-" * int(check_output(["stty", "size"]).split()[1])
+
+def display(note):
+    try:
+        print("\n{}\n{}\n{}".format(note, separator(), open(path(note)).read()))
+    except FileNotFoundError:
+        print("\nCouldn't find note: {}\n{}\n".format(note, separator()))
+
+def cat(notes):
+    if not notes:
+        notes = [core]
+    for note in notes:
+        display(note)
+
 def stripExtension(file):
     return file.rpartition(".")[0]
 
@@ -29,19 +44,28 @@ def getNotes():
             notes.append(stripExtension(file))
     return notes
 
-def list(pattern=""):
+def output(notes):
+    for note in notes:
+        print(note)
+
+def list(pattern):
+    if pattern == None:
+        pattern = ""
+    matches = []
     for note in getNotes():
         if pattern in note:
-            print(note)
+            matches.append(note)
+    return matches
 
 def find(pattern):
+    if pattern == None:
+        pattern = ""
+    matches = []
     for note in getNotes():
         text = open(path(note)).read()
         # Case insensitive search unless there are capitals in pattern
         if pattern.islower():
             text = text.lower()
         if pattern in text:
-            print(note)
-
-def cat(note):
-    print("\n{}\n{}\n{}".format(note, "-" * 80, open(path(note)).read()))
+            matches.append(note)
+    return matches
--- en/cmd.py
+++ en/cmd.py
@@ -1,5 +1,10 @@
 from sys import argv
-from en import default, edit, list, find, cat
+from en import default, edit, list, find, cat, output
+
+def head(args):
+    if args:
+        return args[0]
+    return None
 
 def main():
     if len(argv) <= 1:
@@ -8,20 +13,24 @@ def main():
     command, args = argv[1], argv[2:]
     if command == "open" or command == "o":
         edit(args)
-    elif command == "list" or command == "l":
-        if args:
-            list(args[0])
-        else:
-            list()
-    elif command == "find" or command == "f":
-        if args:
-            find(args[0])
-        else:
-            list()
     elif command == "cat" or command == "c":
-        for note in args:
-            cat(note)
+        cat(args)
+    elif command == "list" or command == "l":
+        output(list(head(args)))
+    elif command == "find" or command == "f":
+        output(find(head(args)))
+    elif command == "list-open" or command == "lo":
+        edit(list(head(args)))
+    elif command == "list-cat" or command == "lc":
+        cat(list(head(args)))
+    elif command == "find-open" or command == "fo":
+        edit(find(head(args)))
+    elif command == "find-cat" or command == "fc":
+        cat(find(head(args)))
     else:
         print("Command not recognised")
 
 # TODO append, prepend lines from single command
+# TODO help text
+# TODO readme
+# TODO publish to pypi
